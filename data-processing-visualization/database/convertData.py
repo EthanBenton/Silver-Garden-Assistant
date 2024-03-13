ff85b43e91e3505f42f225a43a29bce28599ba39
james tieu <jtieu001@odu.edu>
2024-03-13 01:22:43 -0400

    naming
    fixed

--- data-processing-visualization/database/convertData.py
+++ data-processing-visualization/database/convertData.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sqlite3
 
 # change file name accordingly
-inputFile = f"../../data-input-sim/src/sensorData.json"
+inputFile = f"../../data-input-sim/src/sensor_data.json"
 
 dataFrame = pd.read_json(inputFile)
 
