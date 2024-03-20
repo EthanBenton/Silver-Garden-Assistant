1c1826b1aa0904be2833b90a6b91f40240eea6d7
james tieu <jtieu001@odu.edu>
2024-03-20 15:58:33 -0400

    new schema
    new new!

--- data-processing-visualization/database/schema.sql
+++ data-processing-visualization/database/schema.sql
@@ -1,14 +1,9 @@
+/*
 CREATE TABLE IF NOT EXISTS gardenTable (
     gardenID INTEGER PRIMARY KEY,
     gardenName TEXT NOT NULL,
     gardenLocation TEXT NOT NULL UNIQUE,
     gardenSize TEXT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS sensorTable (
-    sensorID INTEGER PRIMARY KEY,
-    sensorType INTEGER NOT NULL,
-    FOREIGN KEY (gardenID) REFERENCES gardenTable(gardenID)
 );
 
 CREATE TABLE IF NOT EXISTS weatherTable (
@@ -17,10 +12,16 @@ CREATE TABLE IF NOT EXISTS weatherTable (
     timestampNum INTEGER NOT NULL,
     numValues INTEGER NOT NULL
 );
+*/
+
+CREATE TABLE IF NOT EXISTS sensorTable (
+    sensorID INTEGER PRIMARY KEY,
+    sensorType INTEGER NOT NULL,
+);
 
 CREATE TABLE IF NOT EXISTS sensorDataTable (
     dataID INTEGER PRIMARY KEY,
     FOREIGN KEY (sensorID) REFERENCES sensorTable(sensorID),
-    timestampNum INTEGER NOT NULL,
+    tStamp INTEGER NOT NULL,
     numValues INTEGER NOT NULL
 );
