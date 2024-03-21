2dd75bbc9db7b34103fba525228b025db6f4b280
EthanBenton <ebent004@odu.edu>
2024-03-21 19:19:08 -0400

    react added + i lost python so just saving stuff

--- user_interface/src/flask/app.py
+++ user_interface/src/flask/app.py
@@ -1,43 +1,37 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='src/flask/static')
 app.secret_key = 'your_secret_key'  # Set a secret key for session encryption
 
+# Homepage
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/ChooseYourPlant', methods=['GET', 'POST'])
-def ChooseYourPlant():
-    if request.method == 'POST':
-        plant = request.form.get('plants')
-        if 'plants' not in session:
-            session['plants'] = []  # Initialize an empty list if it doesn't exist
-        if plant:
-            session['plants'].append(plant)
-    return render_template('ChooseYourPlant.html', plant_list=session.get('plants', []))
-
-@app.route('/delete_plant/<int:index>')
-def delete_plant(index):
-    if 'plants' in session and 0 <= index < len(session['plants']):
-        del session['plants'][index]
-    return redirect(url_for('ChooseYourPlant'))
-
+# Solution statement and how PRIA works + MFCD
 @app.route('/About')
 def About():
     return render_template('About.html')
 
+# Navigate back to homepage
 @app.route('/Home')
 def Home():
     return redirect(url_for('index'))
 
+# Clickable box linking to the graphs
 @app.route('/graphs')
 def graphs():
     return render_template('graphs.html')
 
+# Settings page
 @app.route('/settings')
 def settings():
-    return render_template('settings.html')  # Assuming you have a settings.html file in your templates directory
+    return render_template('settings.html')
+
+# User be able to add/remove plants
+@app.route('/ChooseYourPlant', methods=['GET'])
+def ChooseYourPlant():
+    return send_from_directory('src/flask/static/js/react_app/ItemList', 'index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
