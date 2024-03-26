c690415da0afb54b6b7df53a5fa7ef392ff924b0
EthanBenton <ebent004@odu.edu>
2024-03-26 18:49:17 -0400

    login stuff

--- user_interface/src/frontend/src/App.js
+++ user_interface/src/frontend/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider } from './ThemeContext';
 import Navbar from './NavBar';
 import HomePage from './HomePage';
@@ -7,24 +7,40 @@ import AboutPage from './AboutPage';
 import GraphsPage from './GraphsPage';
 import SettingsPage from './SettingsPage';
 import ChooseYourPlantPage from './ChooseYourPlantPage';
+// import LoginPage from './LoginPage';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    // Check if user is authenticated, e.g., by checking localStorage or cookies
+    // For demonstration, assume isAuthenticated is stored in localStorage
+    const storedAuth = localStorage.getItem('isAuthenticated');
+    if (storedAuth === 'true') {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, []);
+
   return (
-    <ThemeProvider> {}
-    <Router>
-      <Navbar /> {}
-      <Routes>
+    <ThemeProvider>
+      <Router>
+        <Navbar />
+        <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/About" element={<AboutPage />} />
-        <Route path="/graphs" element={<GraphsPage />} />
-        <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/ChooseYourPlant" element={<ChooseYourPlantPage />} />
-      </Routes>
-    </Router>
+          <Route path="/About" element={<AboutPage />} />
+          <Route path="/Graphs" element={<GraphsPage />} />
+          <Route path="/Settings" element={<SettingsPage />} />
+          <Route path="/ChooseYourPlant" element={<ChooseYourPlantPage />} />
+          <Route path="/Home" element={<HomePage />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
-
+// <Route path="/Login" element={<LoginPage />} />
+// <Route path="/" element={isAuthenticated ? <HomePage /> : <Navigate to="/Login" />} />
 export default App;
 
 
