78c456c4d5e5a291f130797e3619e002d89328fb
jmill035 <jmill035@odu.edu>
2024-04-01 13:53:04 -0400

    Adjusted tables in html generation

--- data_processing_visualization/src/htmlData.py
+++ data_processing_visualization/src/htmlData.py
@@ -2,7 +2,7 @@ import json
 import numpy as np
 
 # Open and read json file
-def read_json(sensorData):
+def read_data(sensorData):
     with open(sensorData, 'r') as file:
         data = json.load(file)
         return data
@@ -14,7 +14,7 @@ def processData(data):
 
     # Get data values in aray
     temperature = np.array([entry['temperature'] for entry in sortedData])
-    humidity = np.array([entry]['humidity'] for entry in sortedData)
+    humidity = np.array([entry['humidity'] for entry in sortedData])
     
     # Round data values
     temperature = np.round(temperature).astype(int)
@@ -35,4 +35,71 @@ def processData(data):
             humidityCount[humid] += 1
         else:
             humidityCount[humid] = 1
-    return humid, humidity, humidityCount, temp, temperature, temperatureCount, timestamp
+    return humidity, humidityCount, temperature, temperatureCount, timestamp
+
+def makehtml(humidity, humidityCount, temperature, temperatureCount, timestamp, outputFile = 'Data.html'):
+    with open(outputFile, 'w') as file:
+        #website formatting
+        file.write('<html>\n')
+        file.write('<head>\n')
+        file.write('<title>Processed Data</title>\n')  # Added missing closing tag
+        file.write('<style>\n')
+        file.write('table {\n')
+        file.write('    border-collapse: collapse;\n')
+        file.write('    width: 50%;\n')
+        file.write('}\n')
+        file.write('th, td {\n')
+        file.write('    border: 1px solid black;\n')
+        file.write('    padding: 8px;\n')
+        file.write('    text-align: center;\n')
+        file.write('}\n')
+        file.write('</style>\n')
+        file.write('</head>\n')
+        file.write('<body>\n')
+        file.write('<h1>Processed Data</h1>\n')
+
+        file.write('<div style="display:flex;">\n')
+        file.write('<div style="flex:33%; padding-right: 10px;">\n')
+        file.write('<h2> Temperature</h2>\n')
+        file.write('<table>\n')
+        file.write('<tr><th>Temperature</th><th>Count</th></tr>\n')
+        for temp, count in temperatureCount.items():
+            file.write(f'<tr><td>{temp}</td><td>{count}</td></tr>\n')
+        file.write('</table>\n')
+        file.write(f'<p>Total Temperature Data Points: {sum(temperatureCount.values())}</p>\n')
+        file.write('</div>\n')
+
+        file.write('<div style="flex:33%; padding-right: 10px;">\n')
+        file.write('<h2> Humidity</h2>\n')
+        file.write('<table>\n')
+        file.write('<tr><th>Humidity</th><th>Count</th></tr>\n')
+        for humid, count in humidityCount.items():
+            file.write(f'<tr><td>{humid}</td><td>{count}</td></tr>\n')
+        file.write('</table>\n')
+        file.write(f'<p>Total Humidity Data Points: {sum(humidityCount.values())}</p>\n')
+        file.write('</div>\n')
+
+        file.write('<div style="flex:33%;">\n')
+        file.write('<h2> All Data with Timestamps</h2>\n')
+        file.write('<table>\n')
+        file.write('<tr><th>Temperature</th><th>Humidity</th><th>Timestamp</th></tr>\n')
+        for i in range(len(timestamp)):
+            file.write(f'<tr><td>{temperature[i]}</td><td>{humidity[i]}</td><td>{timestamp[i]}</td></tr>\n')
+        file.write('</table>\n')
+        file.write('</div>\n')
+
+        file.write('</div>\n')
+        file.write('</body>\n')
+        file.write('</html>\n')
+
+if __name__ == '__main__':
+    inputFile = 'data_input_sim\src\sensor_data.json'
+    outputFile = 'Data.html'
+
+    data = read_data(inputFile)
+
+    temperature, temperatureCount, humidity, humidityCount, timestamp = processData(data)
+
+    makehtml(humidity, humidityCount, temperature, temperatureCount, timestamp, outputFile)
+
+    print(f'Data.html has been made')
