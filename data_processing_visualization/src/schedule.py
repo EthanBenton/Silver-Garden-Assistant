30f89dbb0d9687b793eb546c72ff0f8b022a1921
jmill035 <jmill035@odu.edu>
2024-04-10 20:05:33 -0400

    Created rough scheduling function

---
+++ data_processing_visualization/src/schedule.py
@@ -0,0 +1,38 @@
+import json
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import
+train_test_split
+from sklearn.ensemble import
+RandomForestClassifier
+from datetime import datetime
+
+with open('data_input_sim/src/sensor_data.json') as f:
+    data = json.load(f)
+
+each column
+timestamps.append(entry['timestamp'])
+temperatures.append(enty['temperature'])
+humidities.append(entry['humidity'])
+
+df = pd.DataFrame({
+    'timestamp': timestamps,
+    'temperature': temperatures,
+    'humidity': humidities
+})
+
+df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+X = df[['humidity', 'temperature']]
+Y = df['watering_schedule']
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
+
+model = RandomForestClassifier()
+
+model.fit(X_train, Y_train)
+
+prediction = model.predict(X_test)
+
+accuracy = np.mean(prediction == Y_test)
+print(f"Model Accuracy: {accuracy}")
