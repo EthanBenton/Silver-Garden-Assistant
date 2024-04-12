3584edd090834024006d0340f6db9752b5e5b53e
jmill035 <jmill035@odu.edu>
2024-04-12 16:22:25 -0400

    created second unit test for processData

--- data_processing_visualization/test/htmlDataTest.py
+++ data_processing_visualization/test/htmlDataTest.py
@@ -10,30 +10,41 @@ class TestJsonProcess(unittest.TestCase):
         self.sample_data = [
             {
             "timestamp": "2024-03-26 00:00:00",
-            "temperature": 20.81,
-            "humidity": 43.78
+            "temperature": 25.55,
+            "humidity": 60.32
             },
             {
             "timestamp": "2024-03-26 00:10:00",
-            "temperature": 20.18,
-            "humidity": 47.87
+            "temperature": 26.56,
+            "humidity": 65.94
             },
             {
             "timestamp": "2024-03-26 00:20:00",
-            "temperature": 25.54,
-            "humidity": 36.73
+            "temperature": 25.73,
+            "humidity": 63.21
             },
         ]
         with open('example.json', 'w' ) as file:
             json.dump(self.sample_data, file)
-
-    def tearDown(self):
-        import os
-        os.remove('example.json')
     
     def test_read_data(self):
         data = read_data('example.json')
         self.assertEqual(data, self.sample_data)
 
+    def test_processData(self):
+        temperature, temperatureCount, humidity, humidityCount, timestamp = processData(self.sample_data)
+        
+        self.assertEqual(len(temperature), len(self.sample_data))
+        self.assertEqual(len(humidity), len(self.sample_data))
+        self.assertEqual(len(timestamp), len(self.sample_data))
+
+        for temp, humid in zip(temperature, humidity):
+            self.assertFalse(isinstance(temp, float) or isinstance(temp, int))
+            self.assertFalse(isinstance(humid, float) or isinstance(humid, int))
+
+    def tearDown(self):
+        import os
+        os.remove('example.json')
+        
 if __name__ == '__main__':
     unittest.main()
