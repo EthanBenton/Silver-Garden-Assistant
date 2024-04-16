51e37dc51aae0dc3cb58f9be3782b6a41b393fe1
jmill035 <jmill035@odu.edu>
2024-04-16 13:54:05 -0400

    changed the tests to attempt to open, load, and create a data frame

--- data_processing_visualization/test/WateringScheduleTest.py
+++ data_processing_visualization/test/WateringScheduleTest.py
@@ -10,6 +10,31 @@ from WateringSchedule import *
 
 class TestWaterSchedule(unittest.TestCase):
 
+    # Test loading the json
+    def test_load_from_json(self):
+        data = json.load(f)
+        self.assertEqual(len(data), 3)
+        self.assertIn("humidity", data[0])
+        self.assertIn("temperature", data[0])
+        self.assertIn("timestamp", data[0])
+
+    # Test creating a data frame from json
+    def test_create_dataframe(self):
+        test_humidity = [entry['humidity'] for entry in self.sample_data]
+        test_temperature = [entry['temperature'] for entry in self.sample_data]
+        test_timestamp = [datetime.strptime(entry['timestamp'], '%Y-%m-%d %H:%M:%S') for entry in self.sample_data]
+
+    # Create data frame from data
+        df = pd.DataFrame({
+            'humidity': test_humidity,
+            'temperature': test_temperature,
+            'timestamp': test_timestamp
+        })
+        self.assertIsInstance(df, pd.DataFrame)
+        self.assertEqual(len(df), 3)
+        self.assertEqual(list(df.columns), ['humidity', 'temperature', 'timestamp'])
+        self.assertEqual(df['timestamp'].dtype, '<M8[ns]')
+        
     # Test the opening of a test json file
     def setUp(self):
         self.sample_data = [
@@ -28,50 +53,15 @@ class TestWaterSchedule(unittest.TestCase):
                 "temperature": 27.77,
                 "timestamp": "2024-04-11 20:58:49"
             },
-            {
-                "humidity": 61.95,
-                "temperature": 24.37,
-                "timestamp": "2024-04-11 20:59:49"
-            },
-            {
-                "humidity": 56.19,
-                "temperature": 28.81,
-                "timestamp": "2024-04-11 21:00:49"
-            }
         ]
 
-        # Make a sample data frame
-        self.sample_df = pd.DataFrame(self.sample_data)
-        self.sample_df['timestamp'] = pd.to_datetime(self.sample_df['timestamp'])
-        self.sample_df.set_index('timestamp', inplace = True)
+        # Write data to temporary json file
+        with open('sample_data.json', 'w') as f:
+            json.dump(self.sample_data, f)
 
-    # Test if the json file will load
-    def test_load_json(self):
-        self.assertTrue(isinstance(self.sample_data, list))
-
-    # Test if the data frame was made
-    def test_dataframe_creation(self):
-        self.assertTrue(isinstance(sample.data_df, pd.DataFrame))
-
-    # Test the resampling and interpolation
-    def test_resample_and_interpolation(self):
-        resample_df = test_resample_and_interpolation(self.sample_df)
-        self.assertEqual(len(resample_df), 5)
-
-    # Test the watering schedule prediction
-    def test_watering_schedule_prediction(self):
-        X_test = self.sample_df[['temperature', 'humidity']]
-        predictions = predict_watering_schedule(X_test)
-        self.assertEqual(len(predictions), len(self.sample_data_df))
-
-    # Test the watering time estimation
-    def test_watering_time_estimation(self):
-        estimated_time = estimate_watering_time(self.sample_df)
-        self.assertEqual(len(estimated_time), len(self.sample_data_df))
-
-    # Test the the schedule generator
-    def test_watering_schedule_generation(self):
-        generate_watering_schedule_html(self.sample_df)
+        def tearDown(self):
+            import os
+            os.remove('sample_data.json')
 
 if __name__ == '__main__':
     unittest.main()
