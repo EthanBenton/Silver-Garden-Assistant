d0b116b973f4745ce864a5b556fa95e2f3f7eae5
Aamr  Ibrahim <112897120+EvilDuck300@users.noreply.github.com>
2024-04-23 17:04:48 -0400

    webpage updated

--- user_interface/src/frontend/src/GraphsPage4.js
+++ user_interface/src/frontend/src/GraphsPage4.js
@@ -13,7 +13,7 @@ const GraphsPage4 = () => {
       </Link>
       <h1>Graphs</h1>
       <p>Here are some interesting graphs...GraphsPage4</p>
-      <iframe src={`${process.env.PUBLIC_URL}/graphs/sensor_data.html`} title="Graphs" style={{width: '100%', height: '600px', border: 'none'}}></iframe>
+      <iframe src={`${process.env.PUBLIC_URL}/graphs/regression_model(1).html`} title="Graphs" style={{width: '100%', height: '600px', border: 'none'}}></iframe>
     </div>
   );
 };
